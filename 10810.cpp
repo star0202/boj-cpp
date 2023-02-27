80cd8ab6d5fc7b077634a1cb9408abd21bd43e9e
Starcea <stardev.uwu@gmail.com>
2023-02-27 16:08:42 +0900

    refactor(10810): val name

--- 10810.cpp
+++ 10810.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    int a[101] = {}, n, m;
+    int arr[101] = {}, n, m;
     cin >> n >> m;
     while(m--)
     {
         int i, j, k;
         cin >> i >> j >> k;
         for (int x = i - 1; x < j; x++)
-            a[x] = k;
+            arr[x] = k;
     }
     for (int i = 0; i < n; i++)
-        cout << a[i] << ' ';
+        cout << arr[i] << ' ';
 }
